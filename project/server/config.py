c9253002c3812d0f2f86231a5869cfa4817c7ebf
Jim Rippon <jim@cloudscale-consulting.com>
2019-07-22 21:45:59 +0100

    my solution

--- project/server/config.py
+++ project/server/config.py
@@ -2,13 +2,13 @@
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgresql://postgres:@localhost/'
-database_name = 'something'
+postgres_local_base = 'postgresql://postgres:ChangeMe123@localhost/'
+database_name = 'flask_jwt_auth'
 
 
 class BaseConfig:
     """Base configuration."""
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'm0sS!&r6K*f@WESM8cIz')
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -32,6 +32,7 @@ class TestingConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     """Production configuration."""
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'hwm3QBm^WM&%v!0lxga7')
+    #SECRET_KEY = 'my_precious'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///example'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + os.getenv('PG_USER', 'postgres') + ':' + os.getenv('PG_PASSWORD', 'ChangeMe123') + '@' + os.getenv('PG_HOST', 'localhost') + '/' + os.getenv('PG_DB', database_name)
